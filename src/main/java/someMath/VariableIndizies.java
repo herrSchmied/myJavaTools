67b3613a9de205f075ee89d849e5272f2554254c
herrSchmied <johnbernard@hotmail.de>
2025-12-10 20:21:07 +0100

    variable-Indizies start at 1. Corrected.

--- src/main/java/someMath/VariableIndizies.java
+++ src/main/java/someMath/VariableIndizies.java
@@ -12,12 +12,15 @@ import someMath.exceptions.MathException;
 public class VariableIndizies
 {
 
+	//Indizies start at 1!!!
+	private final int minIndex = 1;
 	private Map<Integer, Integer> map = new HashMap<>();
 	private final int size;
-
-	public VariableIndizies(int n)
+	
+	public VariableIndizies(int n) throws MathException
 	{
 
+		if(n<minIndex)throw new MathException("Index space smaller than 1.");
 		this.size = n;
 		//Start with a boring oldIndex = newIndex.
 		for(int m=1;m<n+1;m++)
@@ -29,14 +32,14 @@ public class VariableIndizies
 	public int getNewIndexOf(int oldIndex) throws MathException
 	{
 
-		if(oldIndex>size||oldIndex<0)throw new MathException("No such old index.");
+		if(oldIndex>size||oldIndex<minIndex)throw new MathException("No such old index.");
 		return map.get(oldIndex);
 	}
 
 	public void setNewIndexOf(int oldIndex, int newIndex) throws MathException
 	{
-		if(oldIndex>size||oldIndex<0)throw new MathException("No such old index.");
-		if(newIndex>size||newIndex<0)throw new MathException("No such new index.");
+		if(oldIndex>size||oldIndex<minIndex)throw new MathException("No such old index.");
+		if(newIndex>size||newIndex<minIndex)throw new MathException("No such new index.");
 	
 		map.put(oldIndex, newIndex);
 	}
@@ -44,7 +47,7 @@ public class VariableIndizies
 	public int getOldIndexOf(Integer newIndex) throws MathException
 	{
 
-		if(newIndex>size||newIndex<0)throw new MathException("No such new index.");
+		if(newIndex>size||newIndex<minIndex)throw new MathException("No such new index.");
 
 		for(Integer oldIndex: map.keySet())
 		{
